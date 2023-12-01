1a3796970cd149bb9b76472efb9737a5486cc56d
Julian Januszka <julianj@wolfjawstudios.com>
2023-12-01 13:45:41 -0500

    Attempt pt 2

--- 1/main.go
+++ 1/main.go
@@ -8,6 +8,18 @@ import (
 	"strings"
 )
 
+var numberMap = map[string]string{
+	"one":   "1",
+	"two":   "2",
+	"three": "3",
+	"four":  "4",
+	"five":  "5",
+	"six":   "6",
+	"seven": "7",
+	"eight": "8",
+	"nine":  "9",
+}
+
 func main() {
 	file, err := os.Open("input.txt")
 	if err != nil {
@@ -20,6 +32,26 @@ func main() {
 		var nums []int64
 
 		line := scanner.Text()
+
+		for {
+			earliestIndex := len(line)
+			earliestKey := ""
+
+			for k, _ := range numberMap {
+				index := strings.Index(line, k)
+				if index != -1 && index < earliestIndex {
+					earliestIndex = index
+					earliestKey = k
+				}
+			}
+
+			if earliestKey == "" {
+				break
+			}
+
+			line = strings.Replace(line, earliestKey, numberMap[earliestKey], 1)
+		}
+
 		chars := strings.Split(line, "")
 		for _, char := range chars {
 			num, err := strconv.ParseInt(char, 0, 10)
@@ -28,12 +60,7 @@ func main() {
 			}
 		}
 
-		var val int
-		if len(nums) == 1 {
-			val, err = strconv.Atoi(fmt.Sprintf("%d%d", nums[0], nums[0]))
-		} else {
-			val, err = strconv.Atoi(fmt.Sprintf("%d%d", nums[0], nums[len(nums)-1]))
-		}
+		val, err := strconv.Atoi(fmt.Sprintf("%d%d", nums[0], nums[0]))
 		if err == nil {
 			sum += val
 		}
